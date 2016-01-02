658f03c2d1f04c4fe634a937346333b9a964a688
Muunsyr <Muunsyr@users.noreply.github.com>
2016-01-02 20:17:47 +1100

    Create colourConverter.h

---
+++ colourConverter.h
@@ -0,0 +1,19 @@
+#ifndef COLOURCONVERTER_H
+#define COLOURCONVERTER_H
+
+#define LUMA_R_COEFF 0.30
+#define LUMA_G_COEFF 0.59
+#define LUMA_B_COEFF 0.11
+
+class colourConverter
+{
+public:
+    colourConverter();
+    ~colourConverter();
+    static void hsl_to_rgb(float, float, float, int *);
+    static void hcl_to_rgb(float, float, float, int *);
+private:
+    static float checkedHue(float);
+};
+
+#endif // COLOURCONVERTER_H
