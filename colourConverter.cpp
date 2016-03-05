573b25c9aa2b683b4a59b47872292a5893f57ff8
Muunsyr <Muunsyr@users.noreply.github.com>
2016-03-06 00:16:30 +1100

    Fix the final colour component out of range issue.

--- colourConverter.cpp
+++ colourConverter.cpp
@@ -103,6 +103,7 @@ void colourConverter::hcl_to_rgb(float hue, float chroma, float luma, int * rgb_
 
   // Calculate luma difference
   x = luma - (LUMA_R_COEFF*rprime + LUMA_G_COEFF*gprime + LUMA_B_COEFF*bprime);
+  x /= 3;
   
   rgb_array[0] = (rprime + x) * 255;
   rgb_array[1] = (gprime + x) * 255;
